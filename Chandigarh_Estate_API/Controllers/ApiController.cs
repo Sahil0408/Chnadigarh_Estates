3493491425bec0e8dcfff20f22a2f59278df38b3
Sahil0408 <128016003+Sahil0408@users.noreply.github.com>
2023-05-08 12:27:14 +0530

    API

---
+++ Chandigarh_Estate_API/Controllers/ApiController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Chandigarh_Estates;
+using Chandigarh_Estate_API.Models;
+
+namespace Chandigarh_Estate_API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _Context;
+        public WebApiController(ApplicationDbContext Context)
+        {
+            _Context = Context;
+        }
+
+        //[HttpGet("GetAll")]
+        //public ResponseModel<List<Userdetail>> GetDetails()
+        //{
+        //    List<Userdetail> objList = _Context.Details.ToList();
+
+
+        //    ResponseModel<List<Userdetail>> objModel = new ResponseModel<List<Userdetail>>();
+        //    objModel.IsSuccess = true;
+        //    objModel.StatusCode = 200;
+        //    objModel.Message = "Data Found Sucessfully";
+        //    objModel.Data = objList;
+
+        //    return objModel;
+
+        //}
+
+        //[HttpPost("/Save")]
+        //public ResponseModel<Userdetail> GetDetails(Userdetail obj)
+        //{
+        //    _Context.Details.Add(obj);
+        //    _Context.SaveChanges();
+
+        //    ResponseModel<Userdetail> objModel = new ResponseModel<Userdetail>();
+        //    objModel.IsSuccess = true;
+        //    objModel.StatusCode = 200;
+        //    objModel.Message = "Data Saved Sucessfully";
+        //    objModel.Data = obj;
+
+        //    return objModel;
+        //}
+
+        //[HttpGet("Getusers/{id}")]
+        //public ResponseModel<Userdetail> GetDetails(int id)
+        //{
+        //    Userdetail std = _Context.Details.Where(s => s.UserId == id).SingleOrDefault();
+
+        //    ResponseModel<Userdetail> objModel = new ResponseModel<Userdetail>();
+        //    objModel.IsSuccess = true;
+        //    objModel.StatusCode = 200;
+        //    objModel.Message = "Data found Sucessfully";
+        //    objModel.Data = std;
+
+        //    return objModel;
+        //}
+
+        //[HttpPost("Edit")]
+        //public ResponseModel<Userdetail> EditGetDetails(Userdetail stu)
+        //{
+        //    _Context.Details.Update(stu);
+        //    _Context.SaveChanges();
+
+        //    ResponseModel<Userdetail> objModel = new ResponseModel<Userdetail>();
+        //    objModel.IsSuccess = true;
+        //    objModel.StatusCode = 200;
+        //    objModel.Message = "Data Edited Sucessfully";
+
+        //    return objModel;
+        //}
+
+        //[HttpGet("Delete")]
+        //public ResponseModel<string> DeleteUser(int id)
+        //{
+        //    ResponseModel<string> objModel = new ResponseModel<string>();
+        //    try
+        //    {
+        //        if (id == 0)
+        //        {
+        //            objModel.IsSuccess = false;
+        //            objModel.StatusCode = 500;
+        //            objModel.Message = "Please enter id";
+        //            return objModel;
+        //        }
+        //        Userdetail student = _Context.Details.Where(m => m.UserId == id).SingleOrDefault();
+        //        _Context.Details.Remove(student);
+        //        _Context.SaveChanges();
+
+        //        objModel.IsSuccess = true;
+        //        objModel.StatusCode = 200;
+        //        objModel.Message = "Data deleted Sucessfully";
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        objModel.IsSuccess = false;
+        //        objModel.StatusCode = 500;
+        //        objModel.Message = ex.Message;
+
+        //    }
+
+
+
+        //    return objModel;
+        //}
+
+    }
+}
---
+++ Chandigarh_Estate_API/Models/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Chandigarh_Estates;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Chandigarh_Estate_API.Models;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Login_Page> Logins { get; set; }
+        public DbSet<Registration_Table> Details { get; set; }
+        public DbSet<Country_Table> Countries { get; set; }
+        public DbSet<State_Table> States { get; set; }
+        public DbSet<City_Table> Cities { get; set; }
+        public DbSet<CompanyDetail> Companies { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Login_Page>().ToTable("Login");
+            modelBuilder.Entity<Registration_Table>().ToTable("Registration_Table");
+            modelBuilder.Entity<Country_Table>().ToTable("Country_Table");
+            modelBuilder.Entity<State_Table>().ToTable("State_Table");
+            modelBuilder.Entity<City_Table>().ToTable("City_Table");
+            modelBuilder.Entity<CompanyDetail>().ToTable("CompanyDetail");
+    }
+
+    }
+
+    
